6e4e016da29b90bf68e7430ba4783e8b0a5f4a72
ele-003 <dlwndud003@gmail.com>
2026-09-21 02:35:38 +0900

    fix: 상품 정렬 드롭다운 너비와 펼침 위치 수정

--- src/app/products/_components/ProductToolbar.tsx
+++ src/app/products/_components/ProductToolbar.tsx
@@ -69,7 +69,9 @@ export function ProductToolbar({
           onValueChange={(value) => {
             if (value) onSortChange(value as ProductListSort);
           }}
-          className="w-25"
+          className="w-35 shrink-0 whitespace-nowrap"
+          contentClassName="whitespace-nowrap"
+          alignItemWithTrigger={false}
         >
           {options.map((option) => (
             <SelectItem key={option.value} value={option.value}>
