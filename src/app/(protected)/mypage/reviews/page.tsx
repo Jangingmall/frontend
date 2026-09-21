5d4bcce6e170662d710523a4de4e34fe30663535
DrCloy <alsry0712@gmail.com>
2026-09-21 09:35:16 +0900

    fix: 후기 화면 섹션 간격을 24px로 정정

--- src/app/(protected)/mypage/reviews/page.tsx
+++ src/app/(protected)/mypage/reviews/page.tsx
@@ -68,7 +68,7 @@ export default function MypageReviewsPage() {
   }
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6">
       <ReviewableItemsRow
         nickname={profileQuery.data?.name}
         data={reviewableQuery.data}
